448dba67fe3071a03280709862fda5c9ef20409f
Jaehyeok Kim <rublin322@gmail.com>
2026-03-14 16:15:06 +0900

    Chapter 3

--- Chapter 3/hello.py
+++ Chapter 3/hello.py
@@ -16,13 +16,53 @@
 #     return f"Hello? {who}?"
 
 
-from fastapi import FastAPI
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/hi")
+# def greet(who):
+#     return f"Hello? {who}?"
+
+
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/hi")
-def greet(who):
+@app.post("/hi")
+def greet(who: str = Body(embed=True)):
     return f"Hello? {who}?"
+
+
+# from fastapi import FastAPI, Header
+
+# app = FastAPI()
+
+# @app.get("/hi")
+# def greet(who: str = Header()):
+#     return f"Hello? {who}?"
+
+
+# from fastapi import FastAPI, Header
+
+# app = FastAPI()
+
+# @app.get("/agent")
+# def get_agent(user_agent: str = Header()):
+#     return user_agent
+
+
+# @app.get("/happy")
+# def happy(status_code=200):
+#     return ":)"
+
+
+# from fastapi import Response
+
+# @app.get("/header/{name}/{value}")
+# def header(name: str, value: str, response: Response):
+#     response.headers[name] = value
+#     return "normal body"
 
 if __name__ == "__main__":
     import uvicorn
