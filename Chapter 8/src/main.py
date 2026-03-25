293085b17548dcdbec7fbbfeb4ba8d2de9bfaf00
Jaehyeok Kim <rublin322@gmail.com>
2026-03-25 11:09:44 +0900

    Chapter 8

--- Chapter 8/src/main.py
+++ Chapter 8/src/main.py
@@ -1,15 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
+from web import creature, explorer
 
 app = FastAPI()
 
-@app.get("/")
-def top():
-    return "top here"
-
-@app.get("/echo/{thing}")
-def echo(thing):
-    return f"echoing {thing}"
+app.include_router(creature.router)
+app.include_router(explorer.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
