ce51ad3dcb33c5d830339f2b743be87e327dd078
Jaehyeok Kim <rublin322@gmail.com>
2026-03-26 16:32:28 +0900

    Chapter 15

---
+++ Chapter 15/main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# from fastapi import File
+
+# @app.post("/small")
+# async def upload_small_file(small_file: bytes = File()) -> str:
+#     return f"file size: {len(small_file)}"
+
+
+# from fastapi import UploadFile
+
+# @app.post("/big")
+# async def upload_big_file(big_file: UploadFile) -> str:
+#     return f"file size: {big_file.size}, name: {big_file.filename}"
+
+
+# from fastapi.responses import FileResponse
+
+# @app.get("/small/{name}")
+# async def download_small_file(name):
+#     return FileResponse(name)
+
+
+# from typing import Generator
+# from fastapi.responses import StreamingResponse
+
+# def gen_file(path: str) -> Generator:
+#     with open(file=path, mode="rb") as file:
+#         yield file.read()
+
+# @app.get("/download_big/{name}")
+# async def download_big_file(name: str):
+#     gen_expr = gen_file(path=name)
+#     response = StreamingResponse(
+#         content=gen_expr,
+#         status_code=200,
+#     )
+#     return response
+
+
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
+
+# main.py가 포함된 디렉터리:
+top = Path(__file__).resolve().parent
+
+app.mount("/static", StaticFiles(directory=f"{top}/static", html=True), name="free")
