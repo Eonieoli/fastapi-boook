f1d3413d3106c147fbf8dfc25a08bca0eae90262
Jaehyeok Kim <rublin322@gmail.com>
2026-03-26 14:30:40 +0900

    Chapter 12

---
+++ Chapter 12/src/main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+from web import creature, explorer, user
+
+app = FastAPI()
+
+app.include_router(creature.router)
+app.include_router(explorer.router)
+app.include_router(user.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
