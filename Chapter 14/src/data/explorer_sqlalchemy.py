94b341af8843ebafd61a1033774c1fa969c187fc
Jaehyeok Kim <rublin322@gmail.com>
2026-03-26 16:19:57 +0900

    Chapter 14

---
+++ Chapter 14/src/data/explorer_sqlalchemy.py
@@ -0,0 +1,18 @@
+from sqlalchemy import MetaData, Table, Column, Text
+from sqlalchemy import create_engine, select, Row
+
+engine = create_engine("sqlite:///db/cryptid.db")
+conn = engine.connect()
+meta = MetaData()
+explorer_table = Table(
+    "explorer",
+    meta,
+    Column("name", Text, primary_key=True),
+    Column("country", Text),
+    Column("description", Text),
+)
+
+def get_one(name: str) -> Row | None:
+    stmt = select(explorer_table).where(explorer_table.c.name==name)
+    result = conn.excute(stmt)
+    return result.fetchone()
