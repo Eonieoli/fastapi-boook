16899edddb832c575e5ceb3b1f7c9aaf30536f17
Jaehyeok Kim <rublin322@gmail.com>
2026-03-26 11:14:04 +0900

    Chapter 11

---
+++ Chapter 11/src/service/user.py
@@ -0,0 +1,94 @@
+from datetime import timedelta, datetime
+import os
+from jose import jwt
+import bcrypt
+from model.user import PublicUser, PrivateUser, SignInUser
+
+if os.getenv("CRYPTID_UNIT_TEST"):
+    from fake import user as data
+else:
+    from data import user as data
+
+# --- 새로운 인즌 관련 코드
+
+# SECRET_KEY는 반드시 바꾸고 배포해야 한다!
+SECRET_KEY = "keep-it-secret-keep-it-safe"
+ALGORITHM = "HS256"
+
+def verify_password(plain: str, hash: str) -> bool:
+    """plain을 해시 값과, 데이터베이스의 hash 값과 비교한다"""
+    password_bytes = plain.encode('utf-8')
+    hash_bytes = hash.encode('utf-8')
+    is_valid = bcrypt.checkpw(password_bytes, hash_bytes)
+    return is_valid
+
+def get_hash(plain: str) -> str:
+    """plain의 해시값을 반환한다"""
+    password_bytes = plain.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    return hashed_password.decode('utf-8')
+
+def get_jwt_username(token: str) -> str | None:
+    """JWT 접근 토큰으로부터 username을 반환한다"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        if not (username := payload.get("sub")):
+            return None
+    except jwt.JWTError:
+        return None
+    return username
+
+def get_current_user(token: str) -> PublicUser | None:
+    """OAuth 토큰을 풀어서 PublicUser를 반환한다"""
+    if not (username := get_jwt_username(token)):
+        return None
+    if (user := lookup_user(username)):
+        return user
+    return None
+
+def lookup_user(username: str, is_public=True) -> PublicUser | PrivateUser | None:
+    """데이터베이스에서 username에 매칭되는 User를 반환한다.
+    is_public이 True이면 PublicUser를 반환하고, False이면 PrivateUser를 반환한다.
+    hash 속성은 PrivateUser만 가지고 있다. 비밀번호 인증을 위해 hash 속성이 필요하다.
+    """
+    if (user := data.get_one(username, is_public=is_public)):
+        return user
+    return None
+
+def auth_user(name: str, plain: str) -> PublicUser | PrivateUser | None:
+    """name과 plain 암호로 유저를 인증한다"""
+    if not (user := lookup_user(name, is_public=False)):
+        return None
+    if not verify_password(plain, user.hash):
+        return None
+    return user
+
+def create_access_token(data: dict, expires: timedelta | None = None):
+    """JWT 접근 토큰을 반환한다"""
+    src = data.copy()
+    now = datetime.utcnow()
+    if not expires:
+        expires = timedelta(minutes=15)
+    src.update({"exp": now + expires})
+    encoded_jwt = jwt.encode(src, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# --- CRUD 통과 코드
+def get_all() -> list[PublicUser]:
+    return data.get_all()
+
+def get_one(name) -> PublicUser:
+    return data.get_one(name)
+
+# data.create는 hash 속성을 지닌 PrivateUser를 기대한다.
+# SignInUser의 password를 해시한 hash 속성을 가지고 있는 PrivateUser를 만들어서 전달한다.
+def create(sign_in_user: SignInUser) -> PublicUser:
+    user = PrivateUser(name=sign_in_user.name, hash=get_hash(sign_in_user.password))
+    return data.create(user)
+
+def modify(name: str, user: PublicUser) -> PublicUser:
+    return data.modify(name, user)
+
+def delete(name: str) -> None:
+    return data.delete(name)
---
+++ Chapter 11/src/web/explorer.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException
+from model.explorer import Explorer
+import service.explorer as service
+from error import Duplicate, Missing
+
+router = APIRouter(prefix="/explorer")
+
+@router.get("")
+def get_all() -> list[Explorer]:
+    return service.get_all()
+
+@router.get("/{name}")
+def get_one(name) -> Explorer:
+    try:
+        return service.get_one(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.post("", status_code=201)
+def create(explorer: Explorer) -> Explorer:
+    try:
+        return service.create(explorer)
+    except Duplicate as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.patch("/{name}")
+def modify(name, explorer: Explorer) -> Explorer:
+    try:
+        return service.modify(name, explorer)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.put("/{name}")
+def replace(name, explorer: Explorer) -> Explorer:
+    try:
+        return service.replace(name, explorer)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
+
+@router.delete("/{name}")
+def delete(name: str):
+    try:
+        return service.delete(name)
+    except Missing as exc:
+        raise HTTPException(status_code=404, detail=exc.msg)
